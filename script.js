5a9a531b3b3962bee6555d6f581948e808d8b583
Gaurav Singh <gauravnumber@gmail.com>
2025-05-11 12:46:37 +0530

    feat: enhanced fetch api ability

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-toast("You noticed!");
+import { fetchWrapper } from "./fetch.js  ";
+
+(async () => {
+  const { data, loading, error } = await fetchWrapper(
+    "https://jsonplaceholder.typicode.com/todos/1ll"
+  );
+
+  toast(data?.title);
+  console.log(loading, error);
+})();
